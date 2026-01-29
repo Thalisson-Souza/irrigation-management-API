74ce08567620431b71eafe333b113cca7ece4d86
Thalisson <thalisson.souza@acad.ufsm.br>
2026-01-29 14:17:09 -0300

    chore: api config

--- src/api.js
+++ src/api.js
@@ -1,12 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
 import pivotRoutes from "./routes/pivot.route.js";
 import irrigationRoutes from "./routes/irrigation.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use("/pivots", pivotRoutes);
 app.use("/irrigations", irrigationRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(3030, () => {
   console.log("Server is running localhost:3030");
