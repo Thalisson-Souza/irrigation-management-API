b99ca519e91ccaf8ee5e05e0510aefed08d7e7f9
Thalisson <thalisson.souza@acad.ufsm.br>
2026-01-29 02:47:11 -0300

    feat: add irrigation routes

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 import express from "express";
 import pivotRoutes from "./routes/pivot.route.js";
+import irrigationRoutes from "./routes/irrigation.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/pivots", pivotRoutes);
+app.use("/irrigations", irrigationRoutes);
 
 app.listen(3030, () => {
   console.log("Server is running localhost:3030");
