b686b32573e19b660ad70ea12488e082a170bafc
Thalisson <thalisson.souza@acad.ufsm.br>
2026-01-29 14:16:37 -0300

    chore: irrigations

--- src/controllers/irrigation.controller.js
+++ src/controllers/irrigation.controller.js
@@ -1,16 +1,14 @@
 import * as irrigationService from "../services/irrigation.service.js";
 
-const USER_ID = "user-123";
-
 export async function getUserIrrigations(request, response) {
-  const irrigations = irrigationService.getUserIrrigations(USER_ID);
+  const userId = request.userId;
+  const irrigations = irrigationService.getUserIrrigations(userId);
 
   response.status(200).json(irrigations);
 }
 
 export async function getUserIrrigationById(request, response) {
-  //tirar
-  const userId = USER_ID;
+  const userId = request.userId;
   const irrigationId = request.params.id;
 
   const irrigation = irrigationService.getUserIrrigationById(userId, irrigationId);
@@ -20,12 +18,13 @@ export async function getUserIrrigationById(request, response) {
 }
 
 export async function createIrrigation(request, response) {
+  const userId = request.userId;
   const { pivotId, applicationAmount, irrigationDate } = request.body;
 
   if (!pivotId || !applicationAmount || !irrigationDate)
     return response.status(400).json({ message: "Não foi preenchido campos obrigatórios" });
 
-  const registerIrrigation = irrigationService.createIrrigation(USER_ID, {
+  const registerIrrigation = irrigationService.createIrrigation(userId, {
     pivotId,
     applicationAmount,
     irrigationDate,
@@ -35,7 +34,7 @@ export async function createIrrigation(request, response) {
 }
 
 export async function deleteUserIrrigation(request, response) {
-  const userId = USER_ID;
+  const userId = request.userId;
   const irrigationId = request.params.id;
 
   const removeIrrigation = irrigationService.deleteUserIrrigation(userId, irrigationId);
--- src/routes/irrigation.route.js
+++ src/routes/irrigation.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as irrigationController from "../controllers/irrigation.controller.js";
+import { authToken } from "../middlewares/auth.middleware.js";
 
 const routes = Router();
 
-routes.get("/", irrigationController.getUserIrrigations);
-routes.get("/:id", irrigationController.getUserIrrigationById);
-routes.post("/", irrigationController.createIrrigation);
-routes.delete("/:id", irrigationController.deleteUserIrrigation);
+routes.get("/", authToken, irrigationController.getUserIrrigations);
+routes.get("/:id", authToken, irrigationController.getUserIrrigationById);
+routes.post("/", authToken, irrigationController.createIrrigation);
+routes.delete("/:id", authToken, irrigationController.deleteUserIrrigation);
 
 export default routes;
